71f0c691091f69665664cfe0dff2d9c83136615c
dipesh <dipesh@coutloot.com>
2020-02-01 12:49:56 +0530

    excelValidation function changes

--- app/utility/utils.js
+++ app/utility/utils.js
@@ -2,9 +2,11 @@
 
 const Joi = require("joi");
 const productModel = require("../models/product");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const bulkUploadServices = require("../services/bulkUploadServices");
 
 var utilsFunctions = {
-    testUtil: async function () {
+    excelValidation: async function () {
         const schema = Joi.object()
             .strict()
             .keys({
@@ -20,9 +22,9 @@ var utilsFunctions = {
                 Brand: Joi.string().required(),
                 Service_Type: Joi.number().integer().required(),
                 Image1: Joi.string().required(),
-                Image2: Joi.string(),
-                Image3: Joi.string(),
-                Image4: Joi.string(),
+                Image2: Joi.string().allow(""),
+                Image3: Joi.string().allow(""),
+                Image4: Joi.string().allow(""),
                 Qty: Joi.number().integer().required(),
                 Size: Joi.string().required(),
                 Color: Joi.string().required(),
@@ -35,13 +37,13 @@ var utilsFunctions = {
             Description: "Mahavir sarees  Present.. It is Dark Blue color Kanjivaram silk saree with exclusive thread zari border and zari Rich pallu. Saree has heavy skirt border look. This saree comes with unstitched silk Blue colour blouse. Blouse which is shown on model is only for modeling purpose.",
             Listing_Price: 3750,
             Selling_Price: 750,
-            Category: null,
+            Category: "null",
             Brand: "MAHAVIR ",
             Service_Type: 1,
-            Image1: "https://www.dropbox.com/s/q43vngom87lnamx/free-hm-kash-1023-m-s-retail-original-imafh9cvv8t4wycu.jpeg?dl=0",
-            Image2: "https://www.dropbox.com/s/zcajxdte8fwkqp5/free-hm-kash-1023-m-s-retail-original-imafh9cvkghrphwc.jpeg?dl=0",
-            Image3: "https://www.dropbox.com/s/f8sqpr27625srw3/free-hm-kash-1023-m-s-retail-original-imafh9cvsq7hz2uy.jpeg?dl=0",
-            Image4: "https://www.dropbox.com/s/4wqcodp5ylx2zqh/free-hm-kash-1023-m-s-retail-original-imafh9cvszkws8dz.jpeg?dl=0",
+            Image1: "https://img.yzcdn.cn/upload_files/2020/01/16/FnmCpwnXF6eqRparCTspCPEsR-Wb.png",
+            Image2: "https://img.yzcdn.cn/upload_files/2020/01/16/FgSrGkKBmN3N-WKVecar9R3WC_aF.png",
+            Image3: "",
+            Image4: "",
             Qty: 1,
             Size: "FREE",
             Color: "BLUE",
@@ -66,18 +68,58 @@ var utilsFunctions = {
             "Vendor_SKU": "CGATS120U063"
         }];
         // let newDataToValidate = JSON.parse(JSON.stringify(dataToValidate));
-        dataToValidate.forEach(arrVal => {
-            console.log("inside forEach");
-            result = Joi.validate(arrVal, schema);
+        for (var i = 0; i < dataToValidate.length; i++) {
+            // console.log("inside forEach");
+            result = Joi.validate(dataToValidate[i], schema);
             if (result.error === null) {
-                console.log("No Error");
-                arrVal["isValid"] = true;
+                // console.log("No Error");
+                if (dataToValidate[i].Image1 != '' || dataToValidate[i].Image1 != null) {
+                    var image1 = await this.doesFileExist(dataToValidate[i].Image1);
+                    // console.log('image1---', image1);
+                    if (image1 == false) {
+                        dataToValidate[i].Image1 = "";
+                    }
+                }
+                if (dataToValidate[i].Image2 != '' || dataToValidate[i].Image2 != null) {
+                    var image2 = await this.doesFileExist(dataToValidate[i].Image2);
+                    // console.log('image2---', image2);
+                    if (image2 == false) {
+                        dataToValidate[i].Image2 = "";
+                    }
+                }
+                if (dataToValidate[i].Image3 != '' || dataToValidate[i].Image3 != null) {
+                    var image3 = await this.doesFileExist(dataToValidate[i].Image3);
+                    // console.log('image3---', image3);
+                    if (image3 == false) {
+                        dataToValidate[i].Image3 = "";
+                    }
+                }
+                if (dataToValidate[i].Image4 != '' || dataToValidate[i].Image4 != null) {
+                    var image4 = await this.doesFileExist(dataToValidate[i].Image4);
+                    // console.log('image4---', image4);
+                    if (image4 == false) {
+                        dataToValidate[i].Image4 = "";
+                    }
+                }
+                dataToValidate[i]["isValid"] = true;
             } else {
-                console.log("An Error");
-                arrVal["isValid"] = false;
+                dataToValidate[i]["isValid"] = false;
             }
-        });
-        console.log("dataToValidate---", dataToValidate);
+        }
+        // console.log("dataToValidate---", dataToValidate);
+        let validateUpload = await bulkUploadServices.validationUpload(dataToValidate);
+        return validateUpload;
+    },
+
+    doesFileExist: async function (urlToFile) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('HEAD', urlToFile, false);
+        xhr.send();
+        if (xhr.status == "200") {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 
